43cfaef41891a6fd5ed75e9fced1918fa81d7bf8
shrutivanw <shruti.dalvi@gmail.com>
2017-09-19 08:31:17 -0700

    Adding files
    Exercise definitions and template rb file

---
+++ README.md
@@ -0,0 +1,51 @@
+# Integers and Arrays Interview Practice
+In this assignment, you'll design and implement some of integer number and array manipulation functions that are commonly asked during interviews.
+If you think of multiple approaches to solve the problem, implement the solution which minimizes space complexity. Explain the other approaches, and your decision in comments above the code.
+
+## Exercises
+Design and implement a method for each of the following. Do not use Ruby library provided functionality. You may use `.length`. Share and explain the time and space complexity for each method.
+1. Design and implement a method that accepts two non-negative integers as parameters and that returns the number of digits that match in the two integers. Two digits match if they are equal and have the same position relative to the end of the number (i.e. starting with the ones digit). In other words, the method should compare the last digits of each number, the second-to-last digits of each number, the third-to-last digits of each number, and so forth, counting how many pairs match.
+For example, for  input values of (1072503891, 62530841), the method would compare as follows:
+1 0 7 2 5 0 3 8 9 1
+    | | | | | | | |
+    6 2 5 3 0 8 4 1
+The method should return 4 in this case because 4 of these pairs match
+(2-2, 5-5, 8-8, and 1-1).
+2. Design and implement a method that checks if the digits in the input positive integer forms a palindrome. The method returns true if the digits in the input form a palindrome. The method returns false otherwise.
+For example, 12521 and 4554 are palindromes. 326725 is not a palindrome.
+3. Design and implement a method that computes factorial of the input non-negative integer and returns it. Factorial of a number, 'n' is represented as 'n!' and is defined as follows:
+   - '0!' = '1'
+   - 'n!' = 'n * (n-1)!' if 'n' > 0
+   -    = 'n * (n-1) * (n-2) * ... * 1'
+For example, 3! = 3 * 2 * 1 = 6. 6! = 720.
+4. Design and implement a method to compute the nth fibonacci number in the series starting with 0.
+   - Fibonacci series starting with 0 looks like this: 0 1 1 2 3 5 8 13 21 34 55 89 144 ...
+   - The next number in the series is the sum of the previous two numbers in the series.
+   - For example:
+     - The 0th fibonacci number is 0
+     - The 1st fibonacci number is 1
+     - The 6th fibonacci number is 8
+5. Design and implement a method that takes two integer arrays and returns their intersection. The two input arrays have non repeating elements.
+   - For example, intersection of [2, 3, 4] and [4, 5, 6] is [4]
+   - intersection of [50, 43, 25, 72] and [25, 36, 43, 50, 80] is [50, 25, 43]
+   - intersection of [9, 30, 42] and [56, 34, 90, 32] is []
+6. Design and implement a method that updates the input matrix. The input matrix i.e. two-dimensional array contains only '0's and '1's. If any number in the array is found to be '0', the method updates all the numbers in the corresponding row as well as the corresponding column to be '0'.
+   - For example if the input is:
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 1 1
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 0 1
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 1 1
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 1 0
+should get updated to:
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 0 0
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0 0 0
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 0 0
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0 0 0
+7. Design and implement a method that checks that for the given matrix, where number of rows are equal to number of columns whether the sum of each row matches the sum of the corresponding column i.e. sum
+of numbers in row 'i' is the same as the sum of numbers in column 'i' for 'i' ranging from '0' to 'row.length-1'. If this is the case, the method return true. Otherwise, it return false.
+   - For example if the input is:
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 2 3 4
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;9 5 3 1
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0 3 5 6
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0 8 3 6
+   - the method should return true. (Sum of 0th row and 0th column is 10, sum of 1st row as well as the 1st column is 18 and so on.)
+Update the integers_and_arrays.rb file to implement the above methods.
---
+++ integers_and_arrays.rb
@@ -0,0 +1,367 @@
+# Returns count of digits matching in the two input non-negative integers
+def digit_match(number_1, number_2)
+  puts "NOT IMPLEMENTED"
+  return 0
+end
+
+# Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
+def is_palindrome(number)
+  puts "NOT IMPLEMENTED"
+  return true
+end
+
+# Computes factorial of the input number and returns it
+def factorial(number)
+  puts "NOT IMPLEMENTED"
+  return number
+end
+
+# Computes the nth fibonacci number in the series starting with 0.
+# fibonacci series: 0 1 1 2 3 5 8 13 21 ...
+# e.g. 0th fibonacci number is 0
+# e.g. 1st fibonacci number is 1
+# ....
+# e.g. 6th fibonacci number is 8
+def fibonacci(n)
+  puts "NOT IMPLEMENTED"
+  return n
+end
+
+# Creates a new array to return the intersection of the two input arrays
+def intersection(array_1, array_2)
+  puts "NOT IMPLEMENTED"
+  return []
+end
+
+# Questions on 2D array or matrix
+
+# Updates the input matrix based on the following rules:
+# Assumption/ Given: All numbers in the matrix are 0s or 1s
+# If any number is found to be 0, the method updates all the numbers in the
+# corresponding row as well as the corresponding column to be 0.
+def matrix_convert_to_0(matrix)
+  puts "NOT IMPLEMENTED"
+end
+
+# Checks that for the given matrix, where number of rows are equal to number of columns
+# whether the sum of each row matches the sum of corresponding column i.e. sum
+# of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
+# If this is the case, return true. Otherwise, return false.
+def matrix_check_sum(matrix)
+  puts "NOT IMPLEMENTED"
+end
+
+### END OF METHODS
+puts "Tests for Digit Match"
+count = digit_match(1072503891, 62530841)
+if count != 4
+  puts "BUG!! 4 digits should match for (1072503891, 62530841). Returned value: #{count}"
+end
+count = digit_match(1234, 4321)
+if count != 0
+  puts "BUG!! No digits should match for (1234, 4321). Returned value: #{count}"
+end
+count = digit_match(3862947593, 3862947593)
+if count != 10
+  puts "BUG!! 10 digits should match for (3862947593, 3862947593). Returned value: #{count}"
+end
+puts "End of digit match tests.\n\n"
+
+puts "Tests for Palindrome"
+if is_palindrome(1001) == false
+  puts "BUG!! 1001 is a palindrome."
+end
+if is_palindrome(1234321) == false
+  puts "BUG!! 1234321 is a palindrome."
+end
+if is_palindrome(77) == false
+  puts "BUG!! 77 is a palindrome."
+end
+if is_palindrome(12345) == true
+  puts "BUG!! 12345 is NOT a palindrome."
+end
+if is_palindrome(123421) == true
+  puts "BUG!! 123421 is NOT a palindrome."
+end
+if is_palindrome(15677641) == true
+  puts "BUG!! 15677641 is NOT a palindrome."
+end
+puts "End of Palindrome tests.\n\n"
+
+puts "Tests for Factorial"
+fact = factorial(5)
+if fact != 120
+  puts "BUG!! Factorial of 5 should be 120 and not #{fact}."
+end
+fact = factorial(1)
+if fact != 1
+  puts "BUG!! Factorial of 1 should be 1 and not #{fact}."
+end
+fact = factorial(0)
+if fact != 1
+  puts "BUG!! Factorial of 0 should be 1 and not #{fact}."
+end
+fact = factorial(6)
+if fact != 720
+  puts "BUG!! Factorial of 6 should be 720 and not #{fact}."
+end
+fact = factorial(7)
+if fact != 5040
+  puts "BUG!! Factorial of 7 should be 5040 and not #{fact}."
+end
+puts "End of Factorial tests.\n\n"
+
+puts "Tests for nth fibonacci number."
+# 0 1 1 2 3 5 8 13 21 34 55 89 144
+fib = fibonacci(1)
+if fib != 1
+  puts "BUG!! the 1st fibonacci number is 0 and not #{fib}."
+end
+fib = fibonacci(3)
+if fib != 2
+  puts "BUG!! the 3rd fibonacci number is 2 and not #{fib}."
+end
+fib = fibonacci(8)
+if fib != 21
+  puts "BUG!! the 8th fibonacci number is 21 and not #{fib}."
+end
+fib = fibonacci(11)
+if fib != 89
+  puts "BUG!! the 11th fibonacci number is 89 and not #{fib}."
+end
+fib = fibonacci(12)
+if fib != 144
+  puts "BUG!! the 12th fibonacci number is 144 and not #{fib}."
+end
+fib = fibonacci(6)
+if fib != 8
+  puts "BUG!! the 6th fibonacci number is 8 and not #{fib}."
+end
+puts "End of Fibonacci tests.\n\n"
+
+puts "Tests for intersection of two arrays."
+# Test 1
+array_1 = [70, 90, 34, 21, 78, 42]
+array_2 = []
+expected_intersection = []
+common_elements = intersection(array_1, array_2)
+if common_elements != expected_intersection
+  puts "BUG!! Intersection elements unexpected."
+  print "Array 1: "
+  print array_1
+  print " Array 2: "
+  print array_2
+  puts
+  print "Expected intersection: "
+  print expected_intersection
+  print "Intersection method returned: "
+  print common_elements
+  puts
+end
+# Test 2
+array_1 = [70, 90, 34, 21, 78, 42]
+array_2 = [21, 67, 54, 29, 42, 30]
+expected_intersection = [21, 42]
+common_elements = intersection(array_1, array_2).sort
+if common_elements != expected_intersection
+  puts "BUG!! Intersection elements unexpected."
+  print "Array 1: "
+  print array_1
+  print " Array 2: "
+  print array_2
+  puts
+  print "Expected intersection: "
+  print expected_intersection
+  print "Intersection method returned: "
+  print common_elements
+  puts
+end
+# Test 3
+array_1 = [2, 5, 4, 1, 89]
+array_2 = [89, 5, 4, 1, 2]
+expected_intersection = [1, 2, 4, 5, 89]
+common_elements = intersection(array_1, array_2).sort
+if common_elements != expected_intersection
+  puts "BUG!! Intersection elements unexpected."
+  print "Array 1: "
+  print array_1
+  print " Array 2: "
+  print array_2
+  puts
+  print "Expected intersection: "
+  print expected_intersection
+  print "Intersection method returned: "
+  print common_elements
+  puts
+end
+# Test 4
+array_1 = [92, 2, 5, 4, 1, 89, 90]
+array_2 = [89, 5, 76, 4, 1, 2]
+expected_intersection = [1, 2, 4, 5, 89]
+common_elements = intersection(array_1, array_2).sort
+if common_elements != expected_intersection
+  puts "BUG!! Intersection elements unexpected."
+  print "Array 1: "
+  print array_1
+  print " Array 2: "
+  print array_2
+  puts
+  print "Expected intersection: "
+  print expected_intersection
+  print "Intersection method returned: "
+  print common_elements
+  puts
+end
+puts "End of intersection of two arrays tests.\n\n"
+
+puts "Tests for Matrix convert to zero"
+## helper method for creating and initializing a matrix with 1s
+def initialize_matrix(rows, columns)
+  # create the matrix using the rows and columns
+  matrix = Array.new(rows){Array.new(columns)}
+
+  # initialize the matrix
+  rows.times do |row|
+    columns.times do |column|
+      matrix[row][column] = 1
+    end
+  end
+  return matrix
+end
+# helper method for verifying updated matrix
+def verify_matrix(matrix, rows_array, columns_array)
+  rows = matrix.size
+  columns = matrix[0].size
+
+  rows.times do |row|
+    columns.times do |column|
+      if (rows_array.include?(row) || columns_array.include?(column))
+        if matrix[row][column] != 0
+          puts "BUG!!! matrix[#{row}][#{column}] should be 0"
+          return false
+        end
+      elsif matrix[row][column] != 1
+        puts "BUG!!! matrix[#{row}][#{column}] should be 1"
+        return false
+      end
+    end
+  end
+  return true
+end
+# Test 1
+rows = 3
+columns = 5
+matrix = initialize_matrix(rows, columns)
+matrix[1][3] = 0 # row 1, column 3
+matrix[2][4] = 0 # row 2, column 4
+rows_array = [1, 2]
+columns_array = [3, 4]
+puts "Original:"
+rows.times do |row|
+  print matrix[row]
+  puts
+end
+matrix_convert_to_0(matrix)
+puts "Converted:"
+rows.times do |row|
+  print matrix[row]
+  puts
+end
+verify_matrix(matrix, rows_array, columns_array)
+# Test 2
+rows = 5
+columns = 3
+matrix = initialize_matrix(rows, columns)
+matrix[0][1] = 0 # row 0, column 1
+matrix[1][1] = 0 # row 1, column 1
+matrix[2][1] = 0 # row 2, column 1
+matrix[3][1] = 0 # row 3, column 1
+matrix[4][1] = 0 # row 4, column 1
+rows_array = [0, 1, 2, 3, 4]
+columns_array = [1]
+puts "Original:"
+rows.times do |row|
+  print matrix[row]
+  puts
+end
+matrix_convert_to_0(matrix)
+puts "Converted:"
+rows.times do |row|
+  print matrix[row]
+  puts
+end
+verify_matrix(matrix, rows_array, columns_array)
+# Test 3
+rows = 4
+columns = 4
+matrix = initialize_matrix(rows, columns)
+rows_array = []
+columns_array = []
+puts "Original:"
+rows.times do |row|
+  print matrix[row]
+  puts
+end
+matrix_convert_to_0(matrix)
+puts "Converted:"
+rows.times do |row|
+  print matrix[row]
+  puts
+end
+verify_matrix(matrix, rows_array, columns_array)
+puts "End of matrix convert to zero tests.\n\n"
+
+puts "Tests for Matrix check sum of rows and columns"
+matrix = [[1, 2, 3, 4], # sum of 0th row = 10
+          [9, 5, 3, 1], # sum of 1st row = 18
+          [0, 3, 5, 6], # sum of 2nd row = 14
+          [0, 8, 3, 6]] # sum of 3rd row = 17
+# sums = 10, 18, 14, 17 for columns 0 through 3
+if matrix_check_sum(matrix) == false
+  puts "BUG!! Sums of each row matches the corresponding column in this matrix."
+  rows = matrix.length
+  rows.times do |row|
+    print matrix[row]
+    puts
+  end
+end
+# test 2
+rows = 5
+columns = 5
+matrix = initialize_matrix(rows, columns) # initialize to all 0s
+matrix[0][2] = 4
+matrix[2][0] = 4
+if matrix_check_sum(matrix) == false
+  puts "BUG!! Sums of each row matches the corresponding column in this matrix."
+  rows = matrix.length
+  rows.times do |row|
+    print matrix[row]
+    puts
+  end
+end
+# test 3
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+          [10, 11, 12]]
+if matrix_check_sum(matrix) == true
+  puts "BUG!! Sums of each row does NOT match the corresponding column in this matrix."
+  rows = matrix.length
+  rows.times do |row|
+    print matrix[row]
+    puts
+  end
+end
+# test 3
+matrix = [[1, 10, 1],
+          [2, 3, 12],
+          [9, 4, 9]]
+if matrix_check_sum(matrix) == false
+  puts "BUG!! Sums of each row matches the corresponding column in this matrix."
+  rows = matrix.length
+  rows.times do |row|
+    print matrix[row]
+    puts
+  end
+end
+puts "End of matrix check sum of rows and columns.\n\n"
